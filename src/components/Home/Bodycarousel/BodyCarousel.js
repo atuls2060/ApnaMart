5ab1c6c734f099e94e10b83c05beeee2d02c2d27
Malay-24 <malaybajpai24@gmailcom>
2023-01-19 18:55:25 +0530

    home

---
+++ src/components/Home/Bodycarousel/BodyCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box,Text } from "@chakra-ui/react";
+const BodyCarousel = () => {
+  return (
+    <Box>
+    <Text>hi</Text>
+    </Box>
+  )
+}
+
+export default BodyCarousel
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Box } from "@chakra-ui/react";
 import ImageSlider from "./Carousel/ImageSlider";
 import { SlideData } from "./Carousel/SlideData";
+import MainBody from './Mainbody/MainBody';
+import BodyCarousel from './Bodycarousel/BodyCarousel';
 const Home = () => {
     return (
         <>
         
              <Box w="100%" p={4} color="white">
                 <ImageSlider slides={SlideData} />
-                
+                <MainBody/> 
              </Box>
+             
         </>
     )
 }
